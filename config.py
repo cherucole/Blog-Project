cc1ed4918981247bdb74a3f9f5bae8d049a2912a
cherucole <cheruyotcheruyot@gmail.com>
2018-09-13 14:45:56 +0300

    added migrations and tables

---
+++ config.py
@@ -0,0 +1,44 @@
+#added after changing folder structure
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cherucole:cherucole@localhost/blog'
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cherucole:cherucole@localhost/watchlist_test'
+
+
+
+class ProdConfig(Config):
+    DEBUG= True
+
+
+
+class DevConfig(Config):
+    DEBUG= True
+
+
+
+#added after changing folder structure
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}

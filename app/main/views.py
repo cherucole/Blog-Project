79059d78b277cb6d69e4f89245016f32f5a17c70
cherucole <cheruyotcheruyot@gmail.com>
2018-09-15 10:09:47 +0300

    comment now being stored in database using post id as well

--- app/main/views.py
+++ app/main/views.py
@@ -17,11 +17,21 @@ def index():
 def about():
     return render_template('about.html')
 
-@main.route('/post/<int:post_id>')
+@main.route('/post/<int:post_id>' ,methods=['GET', 'POST'])
 def post(post_id):
-    post = Post.query.filter_by(id=post_id).one()
+    form = CommentForm()
 
-    return render_template('post.html', post=post)
+    post = Post.query.filter_by(id=post_id).one()
+    if form.validate_on_submit():
+        comment = form.comment.data
+
+        new_comment = Comment(comment=comment,user_id=current_user.id, post_id=post_id)
+
+        db.session.add(new_comment)
+        db.session.commit()
+
+
+    return render_template('post.html', post=post, form=form)
 
 @main.route('/add',methods=['GET', 'POST'])
 def add():
@@ -39,26 +49,6 @@ def add():
         db.session.commit()
         return redirect(url_for('.addpost',form=form ))
     return render_template('add.html', form=form)
-
-@main.route('/addpost', methods=['GET', 'POST'])
-def addpost():
-    form = AddPost()
-    if form.validate_on_submit():
-        title = form.title.data
-        subtitle = form.subtitle.data
-        content = form.content.data
-
-        post = Post(title=title, subtitle=subtitle, content=content, user_id=current_user.id, date_posted=datetime.now())
-
-
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for('.addpost',form=form ))
-
-
-    return redirect(url_for('main.index',form=form))
-
-
 
 @main.route('/user/<uname>')
 def profile(uname):

c5ddbaa750f031c363db0260a5f9a8af704735c5
cherucole <cheruyotcheruyot@gmail.com>
2018-09-15 10:56:47 +0300

    formatted comments well to show poster

--- app/models.py
+++ app/models.py
@@ -34,7 +34,7 @@ class User(UserMixin,db.Model):
         return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'{self.username}'
 
 class Post(db.Model):
     __tablename__ = 'posts'

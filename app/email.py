5464269e2c09131154043fcc60e758beac1e6630
cherucole <cheruyotcheruyot@gmail.com>
2018-11-14 22:29:36 +0300

    Formating code

--- app/email.py
+++ app/email.py
@@ -2,9 +2,6 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-
-
-
 def mail_message(subject,template,to,**kwargs):
     sender_email = 'moringamelissa254@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
